0b90d73a9996e6e9e42ec75eaf0b09130307d50c
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2021-10-16 21:47:46 +0100

    Updates

---
+++ precache-manifest.a3dbae77fdb2cea60c1b047af7688522.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e80e89e259f38b52bf65c913b36e5f",
+    "url": "/preview-credit-card/index.html"
+  },
+  {
+    "revision": "e30ded14c3fa2ed20279",
+    "url": "/preview-credit-card/static/css/main.90e1b80d.chunk.css"
+  },
+  {
+    "revision": "c54a75e8a953fb7e03d8",
+    "url": "/preview-credit-card/static/js/2.0ae8a220.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/preview-credit-card/static/js/2.0ae8a220.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e30ded14c3fa2ed20279",
+    "url": "/preview-credit-card/static/js/main.178c5c03.chunk.js"
+  },
+  {
+    "revision": "6abddbc0566bf648d00c",
+    "url": "/preview-credit-card/static/js/runtime-main.458f8799.js"
+  },
+  {
+    "revision": "0841af952c807bdf56455b1addb4c7df",
+    "url": "/preview-credit-card/static/media/IndieFlower-R.0841af95.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/preview-credit-card/static/media/Montserrat-L.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/preview-credit-card/static/media/Montserrat-R.ee653992.ttf"
+  },
+  {
+    "revision": "6db01fa9d74dac090b3ffa714de1ea7e",
+    "url": "/preview-credit-card/static/media/chip.6db01fa9.png"
+  },
+  {
+    "revision": "e67b3b21fc722442cca782e273ab90d0",
+    "url": "/preview-credit-card/static/media/creditcardBack.e67b3b21.jpg"
+  },
+  {
+    "revision": "c62cfabca0d795b7c035e05bc023cadd",
+    "url": "/preview-credit-card/static/media/creditcardFront.c62cfabc.jpg"
+  },
+  {
+    "revision": "fe807bce353d0bc60f09a60409236255",
+    "url": "/preview-credit-card/static/media/mastercard.fe807bce.png"
+  },
+  {
+    "revision": "89225e85b362eb53dc335464430d3915",
+    "url": "/preview-credit-card/static/media/visa.89225e85.png"
+  }
+]);
